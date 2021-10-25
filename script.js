32fee7e71ffcc94d3ab6b38c427941d8c5720e75
ormate99 <ormate99@gmail.com>
2021-10-25 16:04:20 +0200

    hrml js css created

---
+++ script.js
@@ -0,0 +1,11 @@
+function loadEvent() {
+    console.log('the page has loaded');
+
+    document.getElementById("menu-btn").addEventListener("click", function (event){
+        console.log(event.target);
+        event.target.classList.toggle("clicked");
+
+    });
+}
+
+window.addEventListener("load", loadEvent);
