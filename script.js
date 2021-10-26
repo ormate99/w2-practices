a001fe04397ee476536e07c87b5c849edb02f67a
ormate99 <ormate99@gmail.com>
2021-10-26 15:05:14 +0200

    script.js updated

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+function fastLog(text){
+    console.log(text);
+    return"haha";
+}
+
+function buttonClicked(event);{
+    event.target.classList.toggle("clicked");
+
+    const fastLogResult = fastLog("Ezt a szöveget szeretném kilogoltatni");
+
+    console.log(fastLogResult);
+};
+
+
+
+
 function loadEvent() {
     console.log('the page has loaded');
 
-    document.getElementById("menu-btn").addEventListener("click", function (event){
-        console.log(event.target);
-        event.target.classList.toggle("clicked");
 
-    });
-}
+    function toggleClicked(event){
+        event.target.classList.toggle("clicked");
+    }
+};
+
+document.getElementById("menu-btn").addEventListener("click", toggleClicked);
 
 window.addEventListener("load", loadEvent);
